3a868605ec8ea868814ac0c200d77f2b51e6c89d
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-05-24 12:35:55 -0600

    feat(Integration tests): update auth tests

--- src/tests/integration/auth.test.ts
+++ src/tests/integration/auth.test.ts
@@ -9,18 +9,21 @@ describe('Auth API', () => {
   })
 
   it('Login and authenticate token', async () => {
+    const payload = {
+      email: 'example@example.com',
+      password: 'loremipsum'
+    }
+
     const loginResponse = await app.inject({
       method: 'POST',
       url: '/auth',
-      payload: {
-        email: 'example@example.com',
-        password: 'loremipsum'
-      }
+      payload
     })
 
     assert.strictEqual(loginResponse.statusCode, 200)
     const token = loginResponse.json().token as string
     assert.isString(token)
+    app.log.debug({ request: payload, response: { token } })
 
     const authResponse = await app.inject({
       method: 'GET',
@@ -32,5 +35,56 @@ describe('Auth API', () => {
 
     assert.strictEqual(authResponse.statusCode, 200)
     assert.strictEqual(authResponse.payload, 'ok')
+  })
+  it('Invalid email', async () => {
+    const payload = {
+      email: 'example*example.com',
+      password: 'loremipsum'
+    }
+
+    const loginResponse = await app.inject({
+      method: 'POST',
+      url: '/auth',
+      payload
+    })
+
+    assert.strictEqual(loginResponse.statusCode, 400)
+    const response = loginResponse.json()
+    assert.strictEqual(response.message, 'body/email must match format "email"')
+    app.log.debug({ request: payload, response })
+  })
+  it('Invalid password - too short', async () => {
+    const payload = {
+      email: 'example@example.com',
+      password: 'lorem'
+    }
+
+    const loginResponse = await app.inject({
+      method: 'POST',
+      url: '/auth',
+      payload
+    })
+
+    assert.strictEqual(loginResponse.statusCode, 400)
+    const response = loginResponse.json()
+    assert.strictEqual(response.message, 'body/password must NOT have fewer than 8 characters')
+    app.log.debug({ request: payload, response })
+  })
+  it('Unauthorized', async () => {
+    const payload = {
+      email: 'example@example.com',
+      password: 'asjflasjdlfjasldjflajsl'
+    }
+
+    const loginResponse = await app.inject({
+      method: 'POST',
+      url: '/auth',
+      payload
+    })
+
+    assert.strictEqual(loginResponse.statusCode, 401)
+    const response = loginResponse.json()
+    assert.strictEqual(response.message, 'Unauthorized')
+    app.log.debug({ request: payload, response })
   })
 })
