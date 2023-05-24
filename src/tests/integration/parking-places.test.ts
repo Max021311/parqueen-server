499eb7ba1cfc9aacb32de6ae525bf93dfc0d37bb
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-05-24 12:35:22 -0600

    feat(Integrationt tests): test register departure date

--- src/tests/integration/parking-places.test.ts
+++ src/tests/integration/parking-places.test.ts
@@ -12,6 +12,9 @@ describe('Parking places API', () => {
   })
 
   it('Assign parking place', async () => {
+    const payload = {
+      type: 'normal'
+    }
     const res = await app.inject({
       method: 'POST',
       url: '/assign-parking-place',
@@ -19,16 +22,78 @@ describe('Parking places API', () => {
         'content-type': 'application/json',
         authorization: 'Bearer ' + token
       },
-      payload: {
-        type: 'normal',
-        position: [2, 1]
-      }
+      payload
     })
     const ticket = res.json()
+    app.log.debug({ request: payload, response: ticket })
     assert.property(ticket, 'id')
     assert.isNumber(ticket.id)
     assert.property(ticket, 'parking_place')
     assert.isObject(ticket.parking_place)
+  })
+
+  it('Assign parking place and register exit of ticket', async () => {
+    const payload = {
+      type: 'normal'
+    }
+    const resAssignParking = await app.inject({
+      method: 'POST',
+      url: '/assign-parking-place',
+      headers: {
+        'content-type': 'application/json',
+        authorization: 'Bearer ' + token
+      },
+      payload
+    })
+
+    const ticket = resAssignParking.json()
+    app.log.debug({ request: payload, response: ticket })
+    assert.property(ticket, 'id')
+    assert.isNumber(ticket.id)
+    assert.property(ticket, 'parking_place')
+    assert.isObject(ticket.parking_place)
+    assert.property(ticket, 'departure_date')
+    assert.isNull(ticket.departure_date)
+
+    const resTicketExit = await app.inject({
+      method: 'POST',
+      url: `/ticket/${ticket.id}/register-exit`,
+      headers: {
+        authorization: 'Bearer ' + token
+      }
+    })
+
+    const updatedTicket = resTicketExit.json()
+    app.log.debug({ response: updatedTicket })
+    assert.property(updatedTicket, 'id')
+    assert.property(updatedTicket, 'departure_date')
+    assert.isNotNull(updatedTicket.departure_date)
+  })
+
+  it('Assign parking place - full capacity', async () => {
+    const payload = {
+      type: 'special'
+    }
+
+    async function makeRequest () {
+      return app.inject({
+        method: 'POST',
+        url: '/assign-parking-place',
+        headers: {
+          'content-type': 'application/json',
+          authorization: 'Bearer ' + token
+        },
+        payload
+      })
+    }
+
+    await makeRequest()
+
+    const res = await makeRequest()
+    assert.equal(res.statusCode, 503)
+    const response = res.json()
+    app.log.debug({ request: payload, response })
+    assert.equal(response.message, 'Parking places of type special not available')
   })
 
   it('List parking places', async () => {
@@ -40,6 +105,7 @@ describe('Parking places API', () => {
       }
     })
     const parkingPlaces = res.json()
+    app.log.debug({ response: parkingPlaces })
     assert.isArray(parkingPlaces)
     assert.isObject(parkingPlaces[0])
     assert.property(parkingPlaces[0], 'id')
